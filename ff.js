678bf1bd944b8e2bccea3afb1d1815699c217bf1
Dave Pagurek <davepagurek@gmail.com>
2020-05-16 10:35:49 -0700

    Add scaffolding

--- ff.js
+++ ff.js
@@ -2,4 +2,26 @@
 
 const [,, ...args] = process.argv;
 
-console.log(`Args: ${args}`);
+if (args.length === 0) {
+  showHelp();
+} else if (args[0] === 'commit') {
+  console.log('TODO');
+} else if (args[0] === 'branch') {
+  console.log('TODO');
+} else if (args[0] === 'merge') {
+  console.log('TODO');
+} else if (args[0] === 'checkout') {
+  console.log('TODO');
+} else {
+  showHelp();
+}
+
+function showHelp() {
+  console.log('FileFantasy - Version control for HUMANS');
+  console.log('');
+  console.log('Commands:');
+  console.log('\tff commit');
+  console.log('\tff branch');
+  console.log('\tff merge');
+  console.log('\tff checkout');
+}
